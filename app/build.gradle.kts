91e21f10e27f1976defd20512fa249644dc86c7a
hun73877 <hun230402@gmail.com>
2023-04-09 23:30:46 +0900

    update gradle groovy to kts add Libraries

---
+++ app/build.gradle.kts
@@ -0,0 +1,92 @@
+import com.example.mysunflowerstudy.Libraries
+import com.example.mysunflowerstudy.Configuration
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+}
+
+android {
+    namespace = Configuration.applicationId
+
+    compileSdk = Configuration.compileSdk
+    defaultConfig {
+        applicationId = Configuration.applicationId
+
+        minSdk = Configuration.minSdk
+        targetSdk = Configuration.targetSdk
+        versionCode = Configuration.versionCode
+        versionName = Configuration.versionName
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        getByName("debug") {
+            sourceSets {
+                getByName("main") {
+                    java.srcDir(File("build/generated/ksp/debug/kotlin"))
+                }
+            }
+        }
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            sourceSets {
+                getByName("main") {
+                    java.srcDir(File("build/generated/ksp/release/kotlin"))
+                }
+            }
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+
+    // test
+    implementation(Libraries.junit)
+    testImplementation(Libraries.junit)
+    androidTestImplementation(Libraries.androidxJunit)
+    androidTestImplementation(Libraries.androidxEspressoCore)
+
+    // jetbrains
+    implementation(Libraries.kotlinGradlePlugin)
+    implementation(Libraries.kotlinSerialization)
+    implementation(Libraries.coroutinesAndroid)
+    implementation(Libraries.coroutinesCore)
+
+    // google
+    implementation(Libraries.material)
+    implementation(Libraries.gson)
+
+    // android x
+    implementation(Libraries.androidxCore)
+    implementation(Libraries.appcompat)
+    implementation(Libraries.constraintLayout)
+    implementation(Libraries.viewPager2)
+    implementation(Libraries.fragment)
+    implementation(Libraries.livedata)
+    implementation(Libraries.viewmodel)
+    implementation(Libraries.navigation)
+    implementation(Libraries.navigationUi)
+
+    // network
+    implementation(Libraries.okhttp3LoggingInterceptor)
+    implementation(Libraries.retrofit2ConverterGson)
+    implementation(Libraries.retrofit)
+
+    //
+    implementation(Libraries.glide)
+}
--- buildSrc/src/main/java/com/example/mysunflowerstudy/Configuration.kt
+++ buildSrc/src/main/java/com/example/mysunflowerstudy/Configuration.kt
@@ -1,12 +1,14 @@
 package com.example.mysunflowerstudy
 
 object Configuration {
+    const val applicationId = "com.example.mysunflowerstudy"
+
     const val compileSdk = 33
-    const val tagetSdk = 33
+    const val targetSdk = 33
     const val minSdk = 21
     const val majorVersion = 1
     const val minorVersion = 0
     const val patchVersion = 0
     const val versionName = "$majorVersion$minorVersion$patchVersion"
-    const val versionCode = 0
+    const val versionCode = 1
 }
